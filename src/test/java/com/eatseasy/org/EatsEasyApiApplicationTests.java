4ade207b4147805f2f3eb81cdade72d1add3122c
Maxxeehs <maxtig49@outlook.fr>
2024-03-12 21:29:21 +0100

    setup docker-compose file for setup database

--- src/test/java/com/eatseasy/org/EatsEasyApiApplicationTests.java
+++ src/test/java/com/eatseasy/org/EatsEasyApiApplicationTests.java
@@ -1,13 +1,12 @@
 package com.eatseasy.org;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EatsEasyApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
