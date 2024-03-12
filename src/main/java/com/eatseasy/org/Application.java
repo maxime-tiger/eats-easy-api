04a36d0c36fa0d1f315c2caf06c2c72051c67bdd
Maxxeehs <maxtig49@outlook.fr>
2024-03-12 22:12:00 +0100

    Conf initial

--- src/main/java/com/eatseasy/org/Application.java
+++ src/main/java/com/eatseasy/org/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EatsEasyApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EatsEasyApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
